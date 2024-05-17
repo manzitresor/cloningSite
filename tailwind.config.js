8e97a1b32278f6a7ce65b7f33a05640b12130030
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-05-17 11:29:44 +0200

    Add study section

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,30 @@ module.exports = {
       },
       fontFamily: {
         Lato: ["Lato"]
-      }
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          md: "4rem",
+          lg: "6rem",
+          xl: "8rem",
+          "2xl": "12rem",
+          "3xl": "16rem",
+          "4xl": "20rem",
+        },
+        margin: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          md: "4rem",
+          lg: "6rem",
+          xl: "8rem",
+          "2xl": "12rem",
+          "3xl": "16rem",
+          "4xl": "20rem",
+        },
+      },
     },
   },
   plugins: [],
