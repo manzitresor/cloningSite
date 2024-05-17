aedfc10d35a07b36ad449655c16e938525c9a13f
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-05-17 09:14:44 +0200

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#a18a4d',
+        'lightGray': '#999999',
+        'darkgray': '#333333',
+        'middleGray': '#333333e6',
+        'lightTextColor': '#f2f2f2',
+        'lighterGray': '#d5d5d5'
+      },
+    },
   },
   plugins: [],
 }
